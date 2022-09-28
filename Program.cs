da8bee3e1d67b9eba81f1906bfafe0c66cf544e5
BLACKHENZ29 <ayehenz29@gmail.com>
2022-09-28 10:42:02 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*ARRAYS*/
+            string[] names = { "Adebayo", "Mike", "Sophia", "David", "Faithful", "Obinna" };
+
+            Array.Sort(names);
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+        }
+    }
+}
