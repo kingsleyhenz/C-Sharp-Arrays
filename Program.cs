bd68e14fd66590d65eb1d145141d7d2a8cdea6e5
BLACKHENZ29 <ayehenz29@gmail.com>
2022-09-28 10:43:20 +0100

    Learned how to insert and loop through an Array

--- Program.cs
+++ Program.cs
@@ -5,13 +5,28 @@
         static void Main(string[] args)
         {
             /*ARRAYS*/
-            string[] names = { "Adebayo", "Mike", "Sophia", "David", "Faithful", "Obinna" };
+           /* string[] names = { "Adebayo", "Mike", "Sophia", "David", "Faithful", "Obinna" };
 
             Array.Sort(names);
 
             for (int i = 0; i < names.Length; i++)
             {
                 Console.WriteLine(names[i]);
+            }
+           */
+            int[] age = new int[5];
+            age[0] = 16;
+            age[1] = 23;
+            age[2] = 60;
+            age[3] = 2;
+            age[4] = 19;
+
+            Console.WriteLine("Enter your age:");
+            for (int i = 0; i < age.Length; i++)
+            {
+
+                age[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(age[i]);
             }
 
         }
